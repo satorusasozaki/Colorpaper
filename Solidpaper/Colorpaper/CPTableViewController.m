68697a3de47eda14c1977631335c4ccfa0f8e1b7
satorusasozaki <sasozakisatoru@gmail.com>
2016-01-15 20:15:22 -0800

    Add MCSwipeTableViewCell deleteRowsAtIndexPaths throwing exception Switch swiping from the left

--- Solidpaper/Colorpaper/CPTableViewController.m
+++ Solidpaper/Colorpaper/CPTableViewController.m
@@ -12,7 +12,7 @@
 #import "UIImage+withColor.h"
 #import "ColorLists.h"
 #import <URBNAlert/URBNAlert.h>
-
+#import "MCSwipeTableViewCell.h"
 
 @interface CPTableViewController ()
 @property (strong, nonatomic) ColorLists *colorList;
@@ -44,12 +44,45 @@
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     
     static NSString *identifier = @"CELL";
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
+    MCSwipeTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
     if (cell == nil) {
-        cell = [[CPTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier];
+        cell = [[MCSwipeTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier];
     }
     cell.backgroundColor = [self.colorList.list objectAtIndex:indexPath.row];
+    
+    
+    UIView *checkView = [self viewWithImageName:@"check"];
+    UIColor *greenColor = [UIColor colorWithRed:85.0 / 255.0 green:213.0 / 255.0 blue:80.0 / 255.0 alpha:1.0];
+    
+    
+    [cell setDefaultColor:self.tableView.backgroundView.backgroundColor];
+    [cell setSwipeGestureWithView:checkView color:greenColor mode:MCSwipeTableViewCellModeExit state:MCSwipeTableViewCellState1 completionBlock:^(MCSwipeTableViewCell *cell, MCSwipeTableViewCellState state, MCSwipeTableViewCellMode mode) {
+        [self deleteCell:cell];
+        NSLog(@"Did swipe \"Checkmark\" cell");
+    }];
+
+    
+//    UIView *crossView = [self viewWithImageName:@"cross"];
+//    UIColor *redColor = [UIColor colorWithRed:232.0 / 255.0 green:61.0 / 255.0 blue:14.0 / 255.0 alpha:1.0];
+//    [cell setSwipeGestureWithView:crossView color:redColor mode:MCSwipeTableViewCellModeSwitch state:MCSwipeTableViewCellState2 completionBlock:^(MCSwipeTableViewCell *cell, MCSwipeTableViewCellState state, MCSwipeTableViewCellMode mode) {
+//        NSLog(@"Did swipe \"Cross\" cell");
+//    }];
+
+    
+    
     return cell;
+}
+
+- (void)deleteCell:(MCSwipeTableViewCell *)cell {
+    NSIndexPath *indexPath = [self.tableView indexPathForCell:cell];
+    [self.tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+}
+
+- (UIView *)viewWithImageName:(NSString *)imageName {
+    UIImage *image = [UIImage imageNamed:imageName];
+    UIImageView *imageView = [[UIImageView alloc] initWithImage:image];
+    imageView.contentMode = UIViewContentModeCenter;
+    return imageView;
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
@@ -66,23 +99,23 @@
     
 }
 
-- (NSArray *)tableView:(UITableView *)tableView editActionsForRowAtIndexPath:(NSIndexPath *)indexPath {
-    
-    UITableViewRowAction *save = [UITableViewRowAction rowActionWithStyle:UITableViewRowActionStyleDestructive
-                                                                    title:@" Save "
-                                                                    handler:^(UITableViewRowAction *action, NSIndexPath *indexPath) {
-                                                                        UITableViewCell *tappedCell = [tableView cellForRowAtIndexPath:indexPath];
-                                                                        
-                                                                        SEL selector = @selector(onCompleteCapture:didFinishSavingWithError:contextInfo:);
-                                                                        
-                                                                        UIImage *solidImage = [UIImage imageWithColor:tappedCell.backgroundColor];
-                                                                        UIImageWriteToSavedPhotosAlbum(solidImage, self, selector, nil);
-                                                                        
-                                                                        [tableView setEditing:NO animated:YES];
-                                                                    }];
-    save.backgroundColor = [UIColor colorWithHexString:@"007AFF"];
-    return @[save];
-}
+//- (NSArray *)tableView:(UITableView *)tableView editActionsForRowAtIndexPath:(NSIndexPath *)indexPath {
+//    
+//    UITableViewRowAction *save = [UITableViewRowAction rowActionWithStyle:UITableViewRowActionStyleDestructive
+//                                                                    title:@" Save "
+//                                                                    handler:^(UITableViewRowAction *action, NSIndexPath *indexPath) {
+//                                                                        UITableViewCell *tappedCell = [tableView cellForRowAtIndexPath:indexPath];
+//                                                                        
+//                                                                        SEL selector = @selector(onCompleteCapture:didFinishSavingWithError:contextInfo:);
+//                                                                        
+//                                                                        UIImage *solidImage = [UIImage imageWithColor:tappedCell.backgroundColor];
+//                                                                        UIImageWriteToSavedPhotosAlbum(solidImage, self, selector, nil);
+//                                                                        
+//                                                                        [tableView setEditing:NO animated:YES];
+//                                                                    }];
+//    save.backgroundColor = [UIColor colorWithHexString:@"007AFF"];
+//    return @[save];
+//}
 
 #pragma mark - Infinite Scroll
 
@@ -97,24 +130,6 @@
 }
 
 - (void)onCompleteCapture:(UIImage *)screenImage didFinishSavingWithError:(NSError *)error contextInfo:(void *)contextInfo {
-//    NSString *message = @"Saved to camera roll";
-//    if (error) message = @"Failed to save";
-//
-//    UIAlertController *alert = [UIAlertController
-//                                alertControllerWithTitle:@""
-//                                message:message
-//                                preferredStyle:UIAlertControllerStyleAlert];
-//    [self presentViewController:alert animated:YES completion:nil];
-//    
-//    
-//    UIAlertAction *ok = [UIAlertAction
-//                         actionWithTitle:@"OK"
-//                         style:UIAlertActionStyleDefault
-//                         handler:^(UIAlertAction *action) {
-//                             [alert dismissViewControllerAnimated:YES completion:nil];
-//                         }];
-//    [alert addAction:ok];
-
     
     URBNAlertViewController *uac = [[URBNAlertViewController alloc] initWithTitle:@"The color is saved to your camera roll" message:@""];
     uac.alertConfig.touchOutsideViewToDismiss = YES; // Touching outside the alert view will dismiss the alert (only for passive alerts)
@@ -123,10 +138,6 @@
     uac.alertStyler.buttonBackgroundColor = [UIColor colorWithHexString:@"007AFF"];
     [uac addAction:[URBNAlertAction actionWithTitle:@"OK" actionType:URBNAlertActionTypeNormal actionCompleted:^(URBNAlertAction *action) {
         // URBNAlertActionTypeNormal is triggered when the user touches the button specified by this action
-    }]];
-
-    [uac addAction:[URBNAlertAction actionWithTitle:nil actionType:URBNAlertActionTypePassive actionCompleted:^(URBNAlertAction *action) {
-        // URBNAlertActionTypePassive is triggered when the user taps on the actual alert view only for passive. Do something here, ie push a new view controller. For passive alerts only.
     }]];
     
     [uac show];
--- Solidpaper/Pods/Target Support Files/Pods/Pods-frameworks.sh
+++ Solidpaper/Pods/Target Support Files/Pods/Pods-frameworks.sh
@@ -84,10 +84,12 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "Pods/MCSwipeTableViewCell.framework"
   install_framework "Pods/URBNAlert.framework"
   install_framework "Pods/URBNConvenience.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "Pods/MCSwipeTableViewCell.framework"
   install_framework "Pods/URBNAlert.framework"
   install_framework "Pods/URBNConvenience.framework"
 fi
