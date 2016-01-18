85b84105bf18cd12ff0b76ddd022f11516e0be9e
satorusasozaki <sasozakisatoru@gmail.com>
2016-01-17 16:01:15 -0800

    Remove Pods/ from .gitignore file rm -rf Solidpaper.xcodeworkspace pod install

---
+++ Solidpaper/Pods/Target Support Files/URBNAlert/URBNAlert-umbrella.h
@@ -0,0 +1,13 @@
+#import <UIKit/UIKit.h>
+
+#import "URBNAlert.h"
+#import "URBNAlertAction.h"
+#import "URBNAlertConfig.h"
+#import "URBNAlertController.h"
+#import "URBNAlertStyle.h"
+#import "URBNAlertView.h"
+#import "URBNAlertViewController.h"
+
+FOUNDATION_EXPORT double URBNAlertVersionNumber;
+FOUNDATION_EXPORT const unsigned char URBNAlertVersionString[];
+
